6c35bd136ca486d6f47af0dfb7dba71883cedd8d
Paul <paulwharin@gmail.com>
2023-12-07 00:33:53 +0000

    feat: display a retrieved Todo item from Json Placeholder API

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,27 @@
 // make an api call using useEffect
 
+import { useEffect, useState } from "react";
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export const APICall = () => {
+  const [todo, setTodo] = useState<Todo[]>([]);
 
-    // Your code here!
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((payload) => setTodo([payload]));
+  }, []);
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h2>useEffect</h2>
+      <p>{JSON.stringify(todo)}</p>
+    </>
+  );
+};
