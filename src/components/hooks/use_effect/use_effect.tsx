18aef49c273a5109dddea0aff96f1996468e94ee
Paul <paulwharin@gmail.com>
2023-12-07 00:42:13 +0000

    refactor: rename Todo retrieving function to fetchTodo

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -12,7 +12,7 @@ type Todo = {
 export const APICall = () => {
   const [todo, setTodo] = useState<Todo[]>([]);
 
-  const grabTodo = async () => {
+  const fetchTodo = async () => {
     const response = await fetch(
       "https://jsonplaceholder.typicode.com/todos/1"
     );
@@ -27,7 +27,7 @@ export const APICall = () => {
   };
 
   useEffect(() => {
-    grabTodo();
+    fetchTodo();
   }, []);
 
   return (
