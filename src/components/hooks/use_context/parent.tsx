da339edc3347f20276a66e86728baaed4c6349f4
Paul <paulwharin@gmail.com>
2023-12-07 00:16:14 +0000

    feat: add ThemeContext to Parent and refactor Child components to use it

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -1,22 +1,39 @@
-import { useState } from 'react';
-import { Child1 } from './child_1';
-import { Child2 } from './child_2';
+import { createContext, useState } from "react";
+import { Child1 } from "./child_1";
+import { Child2 } from "./child_2";
+
+type ThemeProviderType = {
+  backgroundColor: string;
+  color: string;
+  padding: string;
+  margin: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+export const ThemeContext = createContext<ThemeProviderType>(undefined!);
+
+const pickTheme = (dark: boolean) => ({
+  backgroundColor: dark ? "#333" : "#CCC",
+  color: dark ? "white" : "#333",
+  padding: "2rem",
+  margin: "2rem",
+});
 
 export const Parent = () => {
+  const [darkTheme, setDarkTheme] = useState(true);
 
-    const [darkTheme, setDarkTheme] = useState(true)
+  const toggleTheme = () => {
+    setDarkTheme((prevDarkTheme) => !prevDarkTheme);
+  };
 
-    const toggleTheme = () => {
-        setDarkTheme(prevDarkTheme => !prevDarkTheme)
-    }
-
-    return (
-        <div className='section'>
-            <h2>useContext</h2>
-            <button onClick={toggleTheme}>Toggle Theme</button>
-            <Child1 dark={darkTheme} />
-            <Child2 dark={darkTheme} />
-        </div>
-
-    )
-}
+  return (
+    <ThemeContext.Provider value={pickTheme(darkTheme)}>
+      <div className="section">
+        <h2>useContext</h2>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <Child1 />
+        <Child2 />
+      </div>
+    </ThemeContext.Provider>
+  );
+};
