21b8434a3265939f78923259839d812174c580a3
Paul <paulwharin@gmail.com>
2023-12-09 07:32:35 +0000

    fix: change Task Ids to sstring and use UUID values to avoid Add/Delete conflicts

--- src/components/hooks/use_reducer/todo_app.tsx
+++ src/components/hooks/use_reducer/todo_app.tsx
@@ -1,60 +1,60 @@
-import { useState } from 'react';
-import { AddTask } from './add_task.js';
-import { TaskList } from './task_list.js';
+import { useState } from "react";
+import { AddTask } from "./add_task.js";
+import { TaskList } from "./task_list.js";
 
 export interface Task {
-	id: number;
-	text: string;
-	done: boolean;
+  id: string;
+  text: string;
+  done: boolean;
 }
 
 const initialTasks: Task[] = [
-	{ id: 0, text: 'Visit Kafka Museum', done: true },
-	{ id: 1, text: 'Watch a puppet show', done: false },
-	{ id: 2, text: 'Lennon Wall pic', done: false },
+  { id: crypto.randomUUID(), text: "Visit Kafka Museum", done: true },
+  { id: crypto.randomUUID(), text: "Watch a puppet show", done: false },
+  { id: crypto.randomUUID(), text: "Lennon Wall pic", done: false },
 ];
 
 export function TaskApp() {
-	const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState(initialTasks);
 
-	function handleAddTask(text: string) {
-		setTasks([
-			...tasks,
-			{
-				id: tasks.length,
-				text: text,
-				done: false,
-			},
-		]);
-	}
+  function handleAddTask(text: string) {
+    setTasks([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        text: text,
+        done: false,
+      },
+    ]);
+  }
 
-	function handleChangeTask(updatedTask: Task) {
-		setTasks(
-			tasks.map((t) => {
-				if (t.id === updatedTask.id) {
-					return updatedTask;
-				} else {
-					return t;
-				}
-			})
-		);
-	}
+  function handleChangeTask(updatedTask: Task) {
+    setTasks(
+      tasks.map((t) => {
+        if (t.id === updatedTask.id) {
+          return updatedTask;
+        } else {
+          return t;
+        }
+      })
+    );
+  }
 
-	function handleDeleteTask(taskId: number) {
-		setTasks(tasks.filter((t) => t.id !== taskId));
-	}
+  function handleDeleteTask(taskId: string) {
+    setTasks(tasks.filter((t) => t.id !== taskId));
+  }
 
-	return (
-		<>
-			<h2>useReducer</h2>
+  return (
+    <>
+      <h2>useReducer</h2>
 
-			<h3>Prague Itinerary</h3>
-			<AddTask onAddTask={handleAddTask} />
-			<TaskList
-				tasks={tasks}
-				onChangeTask={handleChangeTask}
-				onDeleteTask={handleDeleteTask}
-			/>
-		</>
-	);
+      <h3>Prague Itinerary</h3>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
 }
